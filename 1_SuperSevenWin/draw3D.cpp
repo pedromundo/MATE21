83af5bc377c6dcfa9d8ac2d1fce9662635e1b8b5
pedromundo <pedrooraimundo@gmail.com>
2016-07-27 17:50:36 -0300

    Including the fluid simulation kernels in the application

--- 1_SuperSevenWin/draw3D.cpp
+++ 1_SuperSevenWin/draw3D.cpp
@@ -16,6 +16,7 @@
 #include <GL/glut.h>
 
 //CUDA includes
+#include <cufft.h>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
 #include <cuda_gl_interop.h>
@@ -29,24 +30,21 @@
 
 using namespace std;
 
+//CUDA STUFF
+cufftHandle planr2c;
+cufftHandle planc2r;
+size_t tPitch = 0;
+struct cudaGraphicsResource *cuda_vbo_resource; // handles OpenGL-CUDA exchange
+
 vector<Object *> model3D;
-
 GLuint axisShader;
-
-GLuint axisVBO[3];
 GLuint modelVBO[3];
-
 GLfloat *colors;
-
 Point bbMin = { FLT_MAX, FLT_MAX, FLT_MAX }, bbMax = { FLT_MIN, FLT_MIN, FLT_MIN },
 bbCenter = { 0.0, 0.0, 0.0 };
 
 GLint winWdth = 1024, winHeight = 768;
-
 GLboolean drawRef = true, run = true, spin = false, randomcolors = false;
-
-GLfloat rotX = 0.0f, rotY = 0.0f;
-
 GLuint InitShader(const GLchar *vShaderFile, const GLchar *fShaderFile);
 
 /// ***********************************************************************
@@ -277,69 +275,6 @@ GLvoid initShaders() {
 	axisShader = InitShader("axisShader.vert", "axisShader.frag");
 }
 
-//CUDA MATRIX MULT
-
-GLvoid doCuda(){
-	// Perform matrix multiplication C = A*B
-	// where A, B and C are NxN matrices
-	GLint N = 16;
-	GLint SIZE = N*N;
-
-	// Allocate memory on the host
-	vector<float> h_A(SIZE);
-	vector<float> h_B(SIZE);
-	vector<float> h_C(SIZE);
-
-	// Initialize matrices on the host
-	for (int i = 0; i < N; i++){
-		for (int j = 0; j < N; j++){
-			h_A[i*N + j] = (float)sin(i);
-			h_B[i*N + j] = (float)cos(j);
-		}
-	}
-
-	// Allocate memory on the device
-	dev_array<float> d_A(SIZE);
-	dev_array<float> d_B(SIZE);
-	dev_array<float> d_C(SIZE);
-
-	d_A.set(&h_A[0], SIZE);
-	d_B.set(&h_B[0], SIZE);
-
-	matrixMultiplication(d_A.getData(), d_B.getData(), d_C.getData(), N);
-	cudaDeviceSynchronize();
-
-	d_C.get(&h_C[0], SIZE);
-	cudaDeviceSynchronize();
-
-	float *cpu_C;
-	cpu_C = new float[SIZE];
-
-	// Now do the matrix multiplication on the CPU
-	float sum;
-	for (int row = 0; row < N; row++){
-		for (int col = 0; col < N; col++){
-			sum = 0.f;
-			for (int n = 0; n < N; n++){
-				sum += h_A[row*N + n] * h_B[n*N + col];
-			}
-			cpu_C[row*N + col] = sum;
-		}
-	}
-
-	float err = 0;
-	// Check the result and make sure it is correct
-	for (int ROW = 0; ROW < N; ROW++){
-		for (int COL = 0; COL < N; COL++){
-			err += cpu_C[ROW * N + COL] - h_C[ROW * N + COL];
-		}
-	}
-
-	cout << "Error: " << err << endl;
-
-	return;
-}
-
 GLint main(GLint argc, GLchar *argv[]) {
 	srand((GLuint)time(NULL));
 
@@ -360,9 +295,7 @@ GLint main(GLint argc, GLchar *argv[]) {
 
 	readModelOBJ("malha.3do", model3D);
 
-	initGL();
-
-	doCuda();
+	initGL();	
 
 	buildModel();
 
