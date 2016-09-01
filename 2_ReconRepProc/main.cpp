9555cdccc7bf8612e0eb2c6abbb38d1b66fac4e6
Pedro Raimundo <pedrooraimundo@gmail.com>
2016-09-01 02:34:43 -0300

    Implemented the reconstrution of a mesh from a point-set using CGAL, next up is projecting a texture.

--- 2_ReconRepProc/main.cpp
+++ 2_ReconRepProc/main.cpp
@@ -1,10 +1,14 @@
+#define CGAL_EIGEN3_ENABLED
 //Old C includes
-#include <cstdio>
-#include <cfloat>
 #include <sys/stat.h>
 
 //Cpp includes
 #include <iostream>
+#include <utility>
+#include <list>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
 //OpenGL Stuff
 #include <glm/gtc/type_ptr.hpp>
@@ -14,10 +18,43 @@
 #include <GL/glut.h>
 #include <SOIL.h>
 
+//CGAL STUff
+#include <CGAL/trace.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/Surface_mesh_default_triangulation_3.h>
+#include <CGAL/make_surface_mesh.h>
+#include <CGAL/Implicit_surface_3.h>
+#include <CGAL/IO/output_surface_facets_to_polyhedron.h>
+#include <CGAL/Poisson_reconstruction_function.h>
+#include <CGAL/Point_with_normal_3.h>
+#include <CGAL/compute_average_spacing.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/pca_estimate_normals.h>
+#include <CGAL/mst_orient_normals.h>
+#include <CGAL/property_map.h>
+#include <CGAL/IO/read_xyz_points.h>
+#include <CGAL/IO/print_wavefront.h>
+
 //My includes
-#include "rply.h"
 #include "myDataStructures.h"
 #include "initShaders.h"
+
+// Types
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::FT FT;
+typedef CGAL::Point_with_normal_3<Kernel> Point_with_normal;
+typedef Kernel::Sphere_3 Sphere;
+typedef std::vector<Point_with_normal> PointList;
+typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
+typedef CGAL::Poisson_reconstruction_function<Kernel> Poisson_reconstruction_function;
+typedef CGAL::Surface_mesh_default_triangulation_3 STr;
+typedef CGAL::Surface_mesh_complex_2_in_triangulation_3<STr> C2t3;
+typedef CGAL::Implicit_surface_3<Kernel, Poisson_reconstruction_function> Surface_3;
+typedef Kernel::Point_3 CGALPoint;
+typedef Kernel::Vector_3 Vector;
+// Point with normal vector stored in a std::pair.
+typedef std::pair<CGALPoint, Vector> PointVectorPair;
 
 void reshape(GLint x, GLint y);
 
@@ -25,10 +62,8 @@ void reshape(GLint x, GLint y);
 GLuint vbo = 0, colorsVBO = 0;                 // OpenGL vertex buffer object
 
 GLboolean g_bExitESC = false;
-
-GLuint axisShader;
 GLubyte *image;
-GLuint wWidth = 1024, wHeight = 768;
+GLuint axisShader, wWidth = 1024, wHeight = 768;
 std::vector<Point>* points = new std::vector<Point>();
 std::vector<Color>* colors = new std::vector<Color>();
 GLint64 nvertices;
@@ -46,14 +81,6 @@ int loadImage(const GLchar* imagePath){
 		return 0;
 	}
 }
-
-//void generateColor() {
-//	chromaKeyingDest = (GLfloat *)malloc(4 * DS * sizeof(GLfloat));
-//
-//	for (GLuint var = 0; var < 4; ++var) {
-//		chromaKeyingDest[var] = (GLfloat)rand() / RAND_MAX;
-//	}
-//}
 
 void shaderPlumbing(){
 	glProgramUniformMatrix4fv(axisShader, glGetUniformLocation(axisShader, "uMVP"), 1, false, glm::value_ptr(MVP));
@@ -74,7 +101,6 @@ void shaderPlumbing(){
 	}
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
 }
 
 float rot = 0.0;
@@ -172,46 +198,8 @@ GLint initGL(GLint *argc, GLchar **argv)
 	return 1;
 }
 
-Point tempPoint = { 0.0, 0.0, 0.0 };
-Color tempColor = { 1.0, 1.0, 1.0, 1.0 };
-static int vertex_cb(p_ply_argument argument) {
-	long idata;
-	ply_get_argument_user_data(argument, NULL, &idata);
-	switch (idata){
-	case 0:
-		tempPoint.x = (GLfloat)ply_get_argument_value(argument);
-		break;
-	case 1:
-		tempPoint.y = (GLfloat)ply_get_argument_value(argument);
-		break;
-	case 2:
-		tempPoint.z = (GLfloat)ply_get_argument_value(argument);
-		points->push_back(tempPoint);
-		tempPoint = { 0.0, 0.0, 0.0 };
-		break;
-	case 3:
-		tempColor.r = (GLfloat)ply_get_argument_value(argument) / 255.0f;
-		break;
-	case 4:
-		tempColor.g = (GLfloat)ply_get_argument_value(argument) / 255.0f;
-		break;
-	case 5:
-		tempColor.b = (GLfloat)ply_get_argument_value(argument) / 255.0f;
-		break;
-	case 6:
-		tempColor.a = (GLfloat)ply_get_argument_value(argument) / 255.0f;
-		colors->push_back(tempColor);
-		tempColor = { 1.0, 1.0, 1.0, 1.0 };
-		break;
-	default:
-		break;
-	}
-	return 1;
-}
-
 GLint main(GLint argc, GLchar **argv)
 {
-	srand(_threadid);
 
 #if defined(__linux__)
 	setenv("DISPLAY", ":0", 0);
@@ -222,29 +210,122 @@ GLint main(GLint argc, GLchar **argv)
 		exit(EXIT_FAILURE);
 	}
 
+	char fname[255];
+	cout << "Enter the name of the .xyz file holding the point set to be reconstructed: ";
+	cin >> fname;
+	std::list<PointVectorPair> points;
+	std::ifstream stream(fname);
+	if (!stream ||
+		!CGAL::read_xyz_points(stream,
+		std::back_inserter(points),
+		CGAL::First_of_pair_property_map<PointVectorPair>()))
+	{
+		std::cerr << "Error: cannot read file " << fname << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	cout << "Estimating normals..." << endl;
+	// Estimates normals direction.	
+	const int nb_neighbors = 18; // K-nearest neighbors = 3 rings
+	CGAL::pca_estimate_normals<CGAL::Sequential_tag>(points.begin(), points.end(),
+		CGAL::First_of_pair_property_map<PointVectorPair>(),
+		CGAL::Second_of_pair_property_map<PointVectorPair>(),
+		nb_neighbors);
+
+	cout << "Orienting normals... (might take a while)" << endl;
+	// Orients normals -- for some reason only works in release mode.	
+	std::list<PointVectorPair>::iterator unoriented_points_begin =
+		CGAL::mst_orient_normals(points.begin(), points.end(),
+		CGAL::First_of_pair_property_map<PointVectorPair>(),
+		CGAL::Second_of_pair_property_map<PointVectorPair>(),
+		nb_neighbors);
+	// Optional: delete points with an unoriented normal
+	// if you plan to call a reconstruction algorithm that expects oriented normals.
+	points.erase(unoriented_points_begin, points.end());
+
+	// Poisson options
+	FT sm_angle = 20.0; // Min triangle angle in degrees.
+	FT sm_radius = 30; // Max triangle size w.r.t. point set average spacing.
+	FT sm_distance = 0.375; // Surface Approximation error w.r.t. point set average spacing.
+
+	PointList poissonPoints = PointList();
+
+	std::list<PointVectorPair>::iterator itePoints;
+
+	for (itePoints = points.begin();
+		itePoints != points.end();
+		itePoints++)
+	{
+		PointVectorPair currentPoint = (*itePoints);
+		poissonPoints.push_back(Point_with_normal(currentPoint.first.x(), currentPoint.first.y(), currentPoint.first.z(), currentPoint.second));
+	}
+
+	cout << "Creating the implicit function..." << endl;
+	// Creates implicit function from the read points using the default solver.
+	Poisson_reconstruction_function function(poissonPoints.begin(), poissonPoints.end(),
+		CGAL::make_normal_of_point_with_normal_pmap(PointList::value_type()));
+
+	cout << "Computing the implicit function for each vertex..." << endl;
+	// Computes the Poisson indicator function f()
+	// at each vertex of the triangulation.
+	if (!function.compute_implicit_function(true)){
+		return EXIT_FAILURE;
+	}
+
+	cout << "Computing average spacing..." << endl;
+	// Computes average spacing
+	FT average_spacing = CGAL::compute_average_spacing<CGAL::Sequential_tag>(poissonPoints.begin(), poissonPoints.end(),
+		6 /* knn = 1 ring */);
+
+	cout << "Computing bounding sphere..." << endl;
+	// Gets one point inside the implicit surface
+	// and computes implicit function bounding sphere radius.
+	CGALPoint inner_point = function.get_inner_point();	
+	Sphere bsphere = function.bounding_sphere();
+	FT radius = std::sqrt(bsphere.squared_radius());
+
+	cout << "Defining the implicit surface..." << endl;
+	// Defines the implicit surface: requires defining a
+	// conservative bounding sphere centered at inner point.
+	FT sm_sphere_radius = 5.0 * radius;
+	FT sm_dichotomy_error = sm_distance*average_spacing / 1000.0; // Dichotomy error must be << sm_distance
+	Surface_3 surface(function,
+		Sphere(inner_point, sm_sphere_radius*sm_sphere_radius),
+		sm_dichotomy_error / sm_sphere_radius);
+
+	// Defines surface mesh generation criteria
+	CGAL::Surface_mesh_default_criteria_3<STr> criteria(sm_angle,  // Min triangle angle (degrees)
+		sm_radius*average_spacing,  // Max triangle size
+		sm_distance*average_spacing); // Approximation error
+
+	cout << "Generating mesh..." << endl;
+	cout << "Enter the desired filename of the output mesh (poisson parameters will be prepended): ";
+	char outName[255];
+	cin >> outName;
+	cout << "Writing to file..." << endl;
+	// Generates surface mesh with manifold option
+	STr tr; // 3D Delaunay triangulation for surface mesh generation
+	C2t3 c2t3(tr); // 2D complex in 3D Delaunay triangulation
+	CGAL::make_surface_mesh(c2t3,             // reconstructed mesh
+		surface,                              // implicit surface
+		criteria,                             // meshing criteria
+		CGAL::Manifold_tag());  // require manifold mesh
+
+	if (tr.number_of_vertices() == 0)
+		return EXIT_FAILURE;
+
+	//Writes reconstructed surface mesh to file
+	stringstream ss;
+	ss << sm_angle << "-" << sm_radius << "-" << sm_distance << "-" << outName;
+	std::ofstream out(ss.str());
+	Polyhedron output_mesh;
+	CGAL::output_surface_facets_to_polyhedron(c2t3, output_mesh);
+	CGAL::print_polyhedron_wavefront(out, output_mesh);
+	out.close();
+	cout << "All done! Displaying..." << endl;
+
 	initShaders();
 
-	p_ply ply = ply_open("bota_pretty_biggur.ply", NULL, 0, NULL);
-
-	if (!ply) return 1;
-	if (!ply_read_header(ply)) return 1;
-	nvertices = ply_set_read_cb(ply, "vertex", "x", vertex_cb, NULL, 0);
-	ply_set_read_cb(ply, "vertex", "y", vertex_cb, NULL, 1);
-	ply_set_read_cb(ply, "vertex", "z", vertex_cb, NULL, 2);
-	ply_set_read_cb(ply, "vertex", "red", vertex_cb, NULL, 3);
-	ply_set_read_cb(ply, "vertex", "green", vertex_cb, NULL, 4);
-	ply_set_read_cb(ply, "vertex", "blue", vertex_cb, NULL, 5);
-	ply_set_read_cb(ply, "vertex", "alpha", vertex_cb, NULL, 6);
-	printf("Vertices: %ld\n", nvertices);
-	if (!ply_read(ply)) return 1;
-	ply_close(ply);
-
-	/*for (std::vector<Color>::iterator it = colors->begin(); it != colors->end(); ++it){
-		printf("{%g, %g, %g}\n", it->r, it->g, it->b);
-		}*/
-
 	glutMainLoop();
-
-	exit(EXIT_SUCCESS);
 	return 0;
 }
